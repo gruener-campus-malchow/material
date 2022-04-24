72e3f7a78018591fbf87e09434bda1ebae3c8a0f
Alexander Baldauf <baldauf@gruener-campus-malchow.de>
2022-04-24 16:41:11 +0200

    gk12info

---
+++ 2022/informatik/gk12/cdocs.php
@@ -0,0 +1,13 @@
+                    <meta charset="utf-8">
+                       **Webseite mit Material der Physik**
+                         Leistungskurs Klasse 13 2021-2022
+                         
+<?=file_get_contents($_GET['md'])?>
+
+<script>markdeepOptions = {mode:'markdeep', tocStyle:'long', definitionStyle:'long', linkAPIDefinitions: false};</script>
+<link rel="stylesheet" href="https://gcm.schule/public/css/markdeep.css">
+<link rel="shortcut icon" href="https://gcm.schule/favicon.ico">
+<link rel="icon" type="image/png" sizes="64x64" href="https://gcm.schule/favicon_64x64.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://gcm.schule/favicon_64x64.png">
+<!-- Markdeep: --><style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style><script src="https://gcm.schule/public/js/markdeep.min.js"></script>><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
+
