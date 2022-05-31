99217229071999d2f486a830ddef4ac82ed2b796
eintyp <eintyp@duck.com>
2022-05-31 17:37:20 +0200

    filter out urls and non-markdown files in cdocs.php

--- cdocs.php
+++ cdocs.php
@@ -1,5 +1,19 @@
-<!doctype html>
+<?php
+$path = $_GET['md'];
+
+// disallow network requests
+// for whatever reason all php file input operations will also operate on URLs with no way to disable it
+if (preg_match('@://@', $path)) die();
+// disallow non-markdown files
+// this is a preventive measure to make sure this script won't print out source code
+if (!preg_match('@\.md$@', $path)) die();
+
+// here's the necessary html tags to load the markdown parser:
+?><!doctype html>
 <meta charset="utf-8">
 <link rel="stylesheet" href="https://gcm.schule/public/css/markdeep.css">
 <script src="https://gcm.schule/public/js/markdeep.min.js"></script>
-<?=file_get_contents($_GET['md'])?>
+<?php
+// load the actual file
+echo file_get_contents($path);
+
