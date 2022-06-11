6c55924719896c1f0ce3e6769389b816ea184c43
eintyp <eintyp@duck.com>
2022-06-11 10:58:58 +0200

    add slides functionality to cdocs.php

--- cdocs.php
+++ cdocs.php
@@ -5,15 +5,50 @@ $path = $_GET['md'];
 // for whatever reason all php file input operations will also operate on URLs with no way to disable it
 if (preg_match('@://@', $path)) die();
 // disallow non-markdown files
-// this is a preventive measure to make sure this script won't print out source code
-if (!preg_match('@\.md$@', $path)) die();
+// this is a preventive measure to make sure this script won't print out source code.
+// yes, manipulation of get parameters should be blocked by the htaccess config, but in this case it's better to be safe than sorry.
+if (!preg_match('@\.md$@', $path) || preg_match('@\?@', $path)) die();
 
-// here's the necessary html tags to load the markdown parser:
+// files ending in ".slides.md" get served as slides
+$is_slides = preg_match('@\.slides\.md$@', $path);
+
+
+
+if ($is_slides):
 ?><!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<link rel="stylesheet" href="https://gcm.schule/slides/public/theme-dark.css">
+		<title>CampusSlides</title>
+	</head>
+	<body>
+		<div class="reveal">
+			<div class="slides">
+				<section data-markdown>
+					<textarea data-template><?=file_get_contents($path)?></textarea>
+				</section>
+			</div>
+		</div>
+		
+		<script src="https://gcm.schule/slides/public/dist/reveal.js"></script>
+		<script src="https://gcm.schule/slides/public/plugin/notes/notes.js"></script>
+		<script src="https://gcm.schule/slides/public/plugin/markdown/markdown.js"></script>
+		<script src="https://gcm.schule/slides/public/plugin/highlight/highlight.js"></script>
+		<script src="https://gcm.schule/slides/public/plugin/math/math.js"></script>
+		<script>
+		Reveal.initialize({
+			hash: true,
+			plugins: [ RevealMarkdown, RevealHighlight, RevealNotes, RevealMath ]
+		});
+		</script>
+	</body>
+</html>
+<?php else: ?><!doctype html>
 <meta charset="utf-8">
 <link rel="stylesheet" href="https://gcm.schule/public/css/markdeep.css">
 <script src="https://gcm.schule/public/js/markdeep.min.js"></script>
-<?php
-// load the actual file
-echo file_get_contents($path);
+<?=file_get_contents($path);?>
+<?php endif;
 
