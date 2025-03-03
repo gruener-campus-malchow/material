9c8d7e0f3d94f49482c60cf260b08fab6b02b1db
ijontychie <baldauf.a@berlin.gcm.schule>
2025-03-03 11:31:43 +0100

    bk11

--- 2025/informatik/bk11/07_Chatbotprogrammierung_in_python.md
+++ 2025/informatik/bk11/07_Chatbotprogrammierung_in_python.md
@@ -3,10 +3,29 @@ Chatbotprogrammierung in Python
 
 Vorbesprechung: Wir haben uns dafür entschieden, keinen Umweg über https://snap.berkeley.edu/ zu machen. Aber zum Selbststudium ist ein [Youtube-Tutorial](https://www.youtube.com/watch?v=kSLnwUuE3Xg) vielleicht hilfreich.
 
-> Erstellen Sie einen mehr oder weniger witzigen Chatbot. Sie können dort gern andere Programme einbinden. Wie das geht sehen wir mit der Zeit.
 
 ## Schritt 1: Verzweigungen und Endlosschleife
 
+> Erstellen Sie einen mehr oder weniger witzigen Chatbot. Sie können dort gern andere Programme einbinden. Wie das geht sehen wir mit der Zeit.
+
+Benutzen Sie dafür eine Endlosschleife, die die komplette Programmroutine ausführt. Verwenden Sie außerdem Verzweigungen, um die Eingaben der Benutzer zu verarbeiten.
+
+### Musterlösung
+
 ![](CHATBOT_Schritt1.png)
 
+## Schritt 2: Strukturierung von Code in Funktionen
 
+In []dieser Python-Datei hier](./rechner.py) befindet sich der Code des Rechners, allerdings ist der angepasst. Es wurde eine main-Funktion hinzugefügt. Diese main-Funktion wird nur dann ausgeführt, wenn man *rechner.py* als eigenständiges Programm ausführt. Wir werden aber diesen Rechner in unserem Chatbot verwenden.
+
+### Verwendung von Funktionen aus anderen Programmen
+
+Der Code des Chatbots wurde angepasst.
+
+![](CHATBOT_Schritt2.png)
+
+Mit der Zeile 1 haben wir die Datei "rechner.py" eingebunden. Dafür müssen beide Python-Dateien, also die Chatbot-Datei und die Rechner-Datei im gleichen Verzeichnis liegen.
+
+In Zeile 12 können wir dann die Funktion addieren() benutzen.
+
+> Ergänzen Sie die Fähigkeit Multiplikation auszuführen.
---
+++ 2025/informatik/bk11/rechner.py
@@ -0,0 +1,34 @@
+def nachfolger(zahl): # Die Funktion namens "nachfolger" wird festgelegt und der Parameter "zahl""
+    return zahl + 1 # zahl wird zurück gegeben und vorher um 1 erhöht
+
+def vorgaenger(zahl):# wie zeile 1 heißt nur anders
+    return zahl - 1 # wie Zeile 2, nur um eins verringert
+
+def addieren(summand1, summand2): # Eine Funktion wird definiert, die zwei Parameter hat
+    while summand2 > 0 : # eine Schleife, die so lange läuft, wie summand2 größer als 0 ist
+        summand1 = nachfolger(summand1) # einer Variable wird der Rückgabewert der Funktion zugewiesen
+        summand2 = vorgaenger(summand2) # wie Zeile 9 nur mit anderer Funktion
+    return summand1 # rückgabe des Inhalts der Variable summand 1
+
+def multiplizieren(faktor1,faktor2): #Die Funktion Multiplizieren hat die Parameter faktor1 und faktor2
+    ergebnis = 0 # Der Variable ergebnis wird der Wert 0 zugewiesen
+    while faktor2 > 0 : #So lange faktor2 größer als 0 ist, wiederhole...
+        ergebnis = addieren(ergebnis,faktor1) #die Funktion addieren wird aufgerufen und der Rückgabewert der Variable ergebnis zugewiesen
+        faktor2 = vorgaenger(faktor2) #Die Funktion vorgaenger wird aufgerufen und der Rückgabewert der Variable faktor2 zugewiesen
+    return ergebnis # Ergebnis wird zurück gegeben 
+
+
+def main():
+    print("gib zwei Zahlen ein") #gib eine Zeichenkette aus
+    var1 = int(input("erste Zahl: ")) #Der Benutzer wird zu einer Eingabe aufgefordert, die in eine Zahl umgewandelt wird. Das Ergebnis wird var1 zugewiesen.
+    var2 = int(input("zweite Zahl: ")) #wie Zeile 21 nur mit var2
+    ergebnis = addieren(var1,var2) #Die Funktion addieren wird aufgerufen und der Rückgabewert der Variable ergebnis zugewiesen
+    print("Die Summe ist") #gib eine Zeichenkette aus
+    print(ergebnis) #Der Inhalt der Variable ergebnis wird ausgegeben
+    ergebnis = multiplizieren(var1,var2) #wie Zeile 23 nur mit Multiplizieren
+    print("Das Produkt ist") #wie Zeile 24, nur mit einer anderen Zeichenkette
+    print(ergebnis) #Der Inhalt der Variable ergebnis wird ausgegeben
+
+if __name__ == "__main__":
+    main()
+
